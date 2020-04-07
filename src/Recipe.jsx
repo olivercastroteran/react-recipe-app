0135976eaeb965c35e2e9dbe1f9b2ddaee260829
olivercastroteran <olivercastroteran@gmail.com>
2020-04-06 22:26:59 -0400

    app styled

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import style from './recipe.module.css';
 
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
+    <div className={style.recipe}>
       <h1>{title}</h1>
       <ol>
         {ingredients.map((ingredient) => (
           <li>{ingredient.text}</li>
         ))}
       </ol>
-      <p>{calories}</p>
-      <img src={image} alt="recipe" />
+      <p>{calories} cal</p>
+      <img src={image} alt="recipe" className={style.image} />
     </div>
   );
 };
